070c40843ec4e89fdfa18fabe0a781fa6ea515e6
Jose Pereira <leaopereira@gmail.com>
2020-12-17 00:11:06 +0100

    Version 1.0.0-beta.1

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-zoft.TinyMvvmExtensions
+# zoft.TinyMvvmExtensions
+
+Extension library built on top of the awesome TinyMvvm framework in a similar I've done in the [MvxExtensions](https://github.com/zleao/MvvmCross-Extensions/tree/master/Libraries/MvxExtensions) library. Provides extra functionalites for ViewModels.
+
+### A bit of context:
+
+This project is part of the **zoft** library, which is nothing more than an attempt to put together a set of C# utilities that I've created in my current and past projects.
+
+A lot of this code comes from another library I've developed over the years ([MvxExtensions](https://github.com/zleao/MvvmCross-Extensions)). This library was initialy built to provide some extensions and utilities that work on top of awesome [MvvmCross](https://github.com/MvvmCross/MvvmCross)... but then it came a time to change and here we are. **zoft** is my attempt to extend the awesome [TinyMvvm](https://github.com/TinyStuff/TinyMvvm) library in the same way I did for MvvmCross. It starts with code ported from MvxExtentions but it will not stop there...
+
+##
+
+###Samples are comming to better explain how to use the library. In the meantime, you can check how to get started with [TinyMvvm](https://github.com/TinyStuff/TinyMvvm). There won't be to much changes to incorporate this library.
---
+++ src/zoft.TinyMvvmExtensions/WeakSubscription/WeakSubscriptionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.ComponentModel;
+
+namespace zoft.TinyMvvmExtensions.WeakSubscription
+{
+    public static class WeakSubscriptionExtensions
+    {
+        public static NotifyPropertyChangedEventSubscription WeakSubscribe(this INotifyPropertyChanged source,
+                                                                           EventHandler<PropertyChangedEventArgs> eventHandler)
+        {
+            return new NotifyPropertyChangedEventSubscription(source, eventHandler);
+        }
+
+        public static NotifyCollectionChangedEventSubscription WeakSubscribe(this INotifyCollectionChanged source,
+                                                                             EventHandler<NotifyCollectionChangedEventArgs> eventHandler)
+        {
+            return new NotifyCollectionChangedEventSubscription(source, eventHandler);
+        }
+
+        public static WeakEventSubscription<TSource> WeakSubscribe<TSource>(this TSource source, string eventName, EventHandler eventHandler)
+            where TSource : class
+        {
+            return new WeakEventSubscription<TSource>(source, eventName, eventHandler);
+        }
+
+        public static WeakEventSubscription<TSource, TEventArgs> WeakSubscribe<TSource, TEventArgs>(this TSource source, string eventName, EventHandler<TEventArgs> eventHandler)
+            where TSource : class
+        {
+            return new WeakEventSubscription<TSource, TEventArgs>(source, eventName, eventHandler);
+        }
+    }
+}
